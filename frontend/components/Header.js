7209059c8d2d3b5f226e78e2ad03428089019592
noamsauerutley <noamsauerutley@gmail.com>
2021-02-06 15:13:25 -0400

    Add heaader skellington

---
+++ frontend/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="bar">Sick Fits</div>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
+    </header>
+  );
+}
