7dc4dea0f0b24038e4fb1a218b5293714fbe959d
noamsauerutley <noamsauerutley@gmail.com>
2021-02-06 13:28:12 -0400

    Add initial basics

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d671e23ae277688086be";
+/******/ 	var hotCurrentHash = "6a165e3248738f157426";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
