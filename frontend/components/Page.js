d4f1f6f534170c7a91497eaaa00ef5a46e18cf61
noamsauerutley <noamsauerutley@gmail.com>
2021-02-06 16:39:04 -0400

    Add GlobalStyles

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,9 +1,24 @@
 import PropTypes from 'prop-types';
+import { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 
+const GlobalStyles = createGlobalStyle`
+  html {
+    --red: #ff1234;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0gba(0, 0, 0, 0,09);
+  }
+`;
 export default function Page({ children, cool }) {
   return (
     <div>
+      <GlobalStyles />
       <Header />
       <h2>I AM YE PAGE COMPONENT</h2>
       <h3>{cool}</h3>
