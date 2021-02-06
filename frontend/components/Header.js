c3d4a965c4ffa8ef79d82f4de42ded1dc5172439
noamsauerutley <noamsauerutley@gmail.com>
2021-02-06 15:38:29 -0400

    Flesh out Header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,7 +1,12 @@
+/* eslint-disable import/no-unresolved */
+import Link from 'next/Link';
+
 export default function Header() {
   return (
     <header>
-      <div className="bar">Sick Fits</div>
+      <div className="bar">
+        <Link href="/">Sick Fits</Link>
+      </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
